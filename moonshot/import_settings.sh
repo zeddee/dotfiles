7977534886ddefe1efea8546c96696837219d9e0
Zed <weizheng.tan@gmail.com>
2018-06-21 20:01:25 +0800

    added more moonshot settings

---
+++ moonshot/import_settings.sh
@@ -0,0 +1,4 @@
+# Import these settings
+
+ln -s "$(pwd)/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/."
+ln -s "$(pwd)/zshrc_moonshot" "$HOME/.zshrc"
